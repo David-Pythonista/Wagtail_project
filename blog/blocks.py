82cb26a3186d9dc238307a9b8961816afa74e4b5
Dávid Molnár <m.david.official@gmail.com>
2022-11-13 17:20:56 +0100

    wagtail blog_website

---
+++ blog/blocks.py
@@ -0,0 +1,17 @@
+from wagtail.blocks import *
+from wagtail.images.blocks import *
+
+class ImageText(StructBlock):
+    reverse = BooleanBlock(required=False)
+    text = RichTextBlock()
+    image = ImageChooserBlock()
+
+
+class BodyBlock(StreamBlock):
+    h1 = CharBlock()
+    h2 = CharBlock()
+    paragraph = RichTextBlock()
+
+    image_text = ImageText()
+    image_carousel = ListBlock(ImageChooserBlock())
+    thumbnail_gallery = ListBlock(ImageChooserBlock())
---
+++ wagtail_bootstrap_blog/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-&0lmtyeoixs#0*q1a7rb(aon41dwb*g9l&d$0t+$r1n(i_9dku"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
